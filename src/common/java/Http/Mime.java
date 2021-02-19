0d88dcba8c2042c451288a46c581ec98f6b344f7
putao520 <yuyao1022@hotmail.com>
2021-02-19 19:13:24 +0800

    优化mine类,优化依赖包

--- src/common/java/Http/Mime.java
+++ src/common/java/Http/Mime.java
@@ -7,28 +7,22 @@ import java.io.File;
 
 public class Mime {
 	public static final String defaultMime = "text/plain";
-	public final String getMime(byte[] buff){
-		String rs;
-		Magic parser = new Magic() ;     
-		MagicMatch match;
+
+	public static final String getMime(byte[] buff) {
 		try {
-            match = Magic.getMagicMatch(buff);
-			rs = match.getMimeType();
+			MagicMatch match = Magic.getMagicMatch(buff);
+			return match.getMimeType();
 		} catch (Exception e) {
-			rs = defaultMime;
-		} 
-		return rs;
+			return defaultMime;
+		}
 	}
-	public final String getMime(File file){
-		String rs;
-		Magic parser = new Magic() ;     
-		MagicMatch match;
+
+	public static final String getMime(File file) {
 		try {
-            match = Magic.getMagicMatch(file, false);
-			rs = match.getMimeType();
+			MagicMatch match = Magic.getMagicMatch(file, true);
+			return match.getMimeType();
 		} catch (Exception e) {
-			rs = defaultMime;
-		} 
-		return rs;
+			return defaultMime;
+		}
 	}
 }
